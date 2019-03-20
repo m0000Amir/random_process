7b708805703651b56f0dda2f796b995fd80f597d
AmirMukhtarov <40074001+AmirMukhtarov@users.noreply.github.com>
2019-03-20 21:34:26 +0300

    Received solution

--- random_process.py
+++ random_process.py
@@ -1,6 +1,4 @@
-import random
 import numpy as np
-from scipy.stats import poisson
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -17,9 +15,9 @@ class InitVar:
 
     def normalized_vector(self):
         """
-        values in the range [0, 1]
+        Values in the range [0, 1]
 
-        sum of all elements is 1
+        Sum of all elements is 1
 
         :return: vector (1 x n)
         """
@@ -36,15 +34,13 @@ class InitVar:
         for i in range(self.num):
             s = self.normalized_vector()
             self.s[i, :] = s
-
         self.lam = np.random.randint(0, 20, size=(1, self.num))
-        # self.lam[0, -1] = 0
 
 
 class RandomProcess:
     def __init__(self, n, init_prob, transit_prob, param):
         """
-        get a random process with a given distribution
+        Get a random process with a given distribution
 
         :param n: nodes number of chain
         :param init_prob: initial probabilities of moving to a node
@@ -68,12 +64,12 @@ class RandomProcess:
         uniform_rand_value = np.random.random()
         prob = np.append(0, prob)
         node = sum(uniform_rand_value >= np.cumsum(prob)) - 1
-        if node == -1:
-            print('Oups')
         return node
 
     def draw(self, pos=None):
         """
+        Drawing of chain
+        :param pos: position of placed nodes
         :return: drawn directed graph
         """
         plt.title('Time accum = ' + str(self.time_accum))
@@ -81,16 +77,22 @@ class RandomProcess:
             pos = nx.spring_layout(self.chain)
 
         nx.draw_networkx(self.chain, pos, with_labels=True,
-                         edge_color='b', node_color='violet',
-                         arrowsize=20, arrowstyle='fancy',
-                         arrows_color='cyan')
+                         edge_color='royalblue',
+                         node_color='mediumorchid', node_size=500,
+                         arrowsize=20, arrowstyle='fancy', arrows_color='cyan')
+
         nx.draw_networkx_nodes(self.chain, pos, node_size=1000,
-                               node_color='r' ,nodelist=[self.node_num - 1])
+                               node_color='indigo',
+                               nodelist=[self.node_num - 1])
         plt.show()
         plt.clf()
         return pos
 
     def solution(self):
+        """
+
+        :return: received time accum
+        """
         [self.chain.add_node(i) for i in range(self.node_num)]
         pos = self.draw()
         node = self.rand_node(self.init_prob)
@@ -107,14 +109,12 @@ class RandomProcess:
 
             self.time_accum += np.random.poisson(self.param[0, node])
             self.draw(pos)
-
-        self.draw(pos)
-
+        # self.draw(pos)
         print('Time accum = ' + str(self.time_accum))
 
 
 if __name__ == '__main__':
-    n = 30
+    n = 30  # nodes number
 
     init = InitVar(n)
     init.create()
